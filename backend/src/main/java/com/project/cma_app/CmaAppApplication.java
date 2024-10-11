bce2e357579dd9c613faea59c54b0cb73194170f
Filipe Lima <fhemrique55@gmail.com>
2024-10-11 16:56:16 -0300

    Adicionando backend spring

---
+++ backend/src/main/java/com/project/cma_app/CmaAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.cma_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmaAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/cma_app/CmaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cma_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
