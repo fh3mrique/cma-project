7eb89a22e41377083e329f7988407a9605513ffb
Filipe Lima <fhemrique55@gmail.com>
2024-10-20 23:37:11 -0300

    feat: parcial login page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 const routes: Routes = [
   {
     path: '',
     title: 'Home',
     pathMatch: 'full',
-    redirectTo: 'home'
+    redirectTo: 'login'
   },
   {
     path: 'home',
     title: 'Home',
     component: HomePageComponent
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent
   }
 ];
 
--- frontend/src/app/pages/pages.module.ts
+++ frontend/src/app/pages/pages.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComponentsModule } from '../components/components.module';
 import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 
 
 @NgModule({
     declarations: [
-        HomePageComponent
+        HomePageComponent,
+        LoginPageComponent
     ],
     imports: [
         CommonModule,
