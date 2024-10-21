23cd3058faf14dec4e8436f37bc63109ffa0937e
Filipe Lima <fhemrique55@gmail.com>
2024-10-20 22:13:27 -0300

    feat: home page

--- frontend/src/app/components/components.module.ts
+++ frontend/src/app/components/components.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { ButtonHomeComponent } from './button-home/button-home.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    ButtonHomeComponent
   ],
   imports: [
     CommonModule,
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    ButtonHomeComponent
   ]
 })
 export class ComponentsModule { }
