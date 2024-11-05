aa2d819ef60b05c23e584c1e11eca619cb40c707
Filipe Lima <fhemrique55@gmail.com>
2024-11-04 21:01:26 -0300

    feat: questions.ts

---
+++ frontend/src/app/components/questionnaire/questions.ts
@@ -0,0 +1,13 @@
+export const QUESTIONS = [
+    {
+      id: 1,
+      text: "Preciso encontrar o caminho para uma loja recomendada por um amigo. Eu:",
+      options: [
+        { letter: 'a', answer: 'Descobriria a localização da loja em relação a um local que conheço.' },
+        { letter: 'b', answer: 'Pediria ao meu amigo as informações sobre como chegar.' },
+        { letter: 'c', answer: 'Anotaria as informações sobre as ruas que preciso para lembrar.' },
+        { letter: 'd', answer: 'Usaria um mapa.' }
+      ]
+    },
+    // Adicione as outras perguntas
+  ];
