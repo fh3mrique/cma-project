4c759222ba9c7be909e7488bc3492c56cf1c9aa7
Filipe Lima <fhemrique55@gmail.com>
2024-11-07 19:43:56 -0300

    fix: deleting import

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { PrincipalHomeComponent } from './pages/principal-home/principal-home.component';
-import { QuestionnaireComponent } from './components/questionnaire/questionnaire.component';
 import { QuestionsPageComponent } from './pages/questions-page/questions-page.component';
 
 const routes: Routes = [
