ea76ebdfcc5d3ff16ac5d4e9984a5c0a119348f5
Filipe Lima <fhemrique55@gmail.com>
2024-11-19 18:36:04 -0300

    feat: questionnare tree

--- frontend/src/app/pages/principal-home/principal-home.component.ts
+++ frontend/src/app/pages/principal-home/principal-home.component.ts
@@ -1,4 +1,27 @@
 import { Component } from '@angular/core';
+import { FlatTreeControl } from '@angular/cdk/tree';
+import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree';
+
+interface TreeNode {
+  name: string;
+  children?: TreeNode[];
+}
+
+interface FlatTreeNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+}
+
+const TREE_DATA: TreeNode[] = [
+  {
+    name: 'Questionários',
+    children: [
+      { name: 'VARK' },
+      { name: 'Honey-Alonso' }
+    ]
+  }
+];
 
 @Component({
   selector: 'app-principal-home',
@@ -8,4 +31,31 @@ import { Component } from '@angular/core';
 export class PrincipalHomeComponent {
   showFiller = true;
 
+  private _transformer = (node: TreeNode, level: number): FlatTreeNode => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node.name,
+      level: level,
+    };
+  };
+
+  treeControl = new FlatTreeControl<FlatTreeNode>(
+    node => node.level,
+    node => node.expandable
+  );
+
+  treeFlattener = new MatTreeFlattener(
+    this._transformer,
+    node => node.level,
+    node => node.expandable,
+    node => node.children
+  );
+
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+  constructor() {
+    this.dataSource.data = TREE_DATA;
+  }
+
+  hasChild = (_: number, node: FlatTreeNode) => node.expandable;
 }
